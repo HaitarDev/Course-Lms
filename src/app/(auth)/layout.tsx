1e5288bc550e5ab3ea9ccfd1a59f1a837f134d0d
azedqsaze <sadm3979@gmail.com>
2024-03-09 14:59:28 +0000

    change BG

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,11 @@
 import { PropsWithChildren } from "react";
 
 function AuthLayout({ children }: PropsWithChildren) {
-  return <div className="h-full w-full flex  justify-center">{children}</div>;
+  return (
+    <div className="relative h-full w-full flex  justify-center">
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <main>{children}</main>
+    </div>
+  );
 }
 export default AuthLayout;
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -7,7 +7,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="h-full overflow-hidden">
+    <div className="relative h-full overflow-hidden">
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+
       <Navbar />
       <main className="h-full flex">
         <SideBar />
